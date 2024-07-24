37e3bc4f87515c6ee408fef6f3dfa97fd373ab01
drAshcroft <brian.a.ashcroft@gmail.com>
2024-07-24 15:45:26 -0700

    Add in old fftw lib

--- src/MathHelps/FFT/MathHelpsFFT.cs
+++ src/MathHelps/FFT/MathHelpsFFT.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Runtime.InteropServices;
-using fftwlib;
+ 
 using System.Reflection;
 using System.IO;
 using System.Windows.Forms;
